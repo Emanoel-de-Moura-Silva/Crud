d28424e150a046ad0fc78ec08887ff7defe3032f
EMANOEL de Moura Silva B901175 <B901175@bnb.gov.br>
2023-02-14 16:48:44 -0300

    erro de rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Component, React } from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Menu from './components/Menu';
 import TabelaLivros from './components/TabelaLivros';
@@ -30,13 +30,42 @@ class App extends Component {
       }
     ]
   };
+    inserirLivro = (livro) => {
+      livro.id = this.state.livros.length + 1;
+      this.setState({
+        livros: [...this.state.livros, livro]
+      });
+    };
+    editarLivro = (livro) =>{
+      const index = this.state.livros.findIndex((p) => p.id === livro.id);
+      const livros = this.state.livros.slice(0, index).concat(this.state.livros.slice(index+1));
+      const newLivros = [...livros,livro].sort((a,b) => a.id - b.id);
+      this.setState({
+        livros: newLivros
+      })
+    }
   render(){
   return (
     <Router>
       <Menu />
       <Routes>
-        <Route path='/' element={<TabelaLivros livros={this.state.livros}/>}/>
-        <Route path='/cadastrar' element={<CadastrarLivros />}/>
+        <Route exact path='/' element={<TabelaLivros livros={this.state.livros}/>}/>
+        <Route exact path='/cadastrar' element={<CadastrarLivros 
+        inserirLivro = {this.inserirLivro}
+        livro = {{id: 0 , isbn: "", titulo: "",autor: ""}} />}/>
+      
+  <Route path="/editar/:isbnLivro" element={
+    ({ params }) => {
+      const livro = this.state.livros.find((livro) => livro.isbn === params.isbnLivro);
+      if(livro){
+        return (
+          React.createElement(CadastrarLivros, { editarLivro: this.editarLivro, livro: livro })
+        )
+      } else {
+        return React.createElement(Navigate, { to: "/" });
+      }
+    }
+  } />
         <Route component= {NotFound}/>
       </Routes>
     </Router>
